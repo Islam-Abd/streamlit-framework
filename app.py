9fcba01c53deaa0d4d33adf316a178241e51648a
Islam-Abd <90225596+Islam-Abd@users.noreply.github.com>
2021-09-17 20:27:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,29 +14,16 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
 
-stock = st.sidebar.text_input("Stock Ticker", 'aapl')
-ticker=stock
-apikey=os.getenv("apikey")
-
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&apikey=%s&outputsize=full'%(ticker,apikey)
-r=requests.get(url)
-data=r.json()
-
-df=pd.DataFrame(data['Time Series (Daily)'])
-close=df.loc['4. close',:]
-close.index=pd.to_datetime(close.index,)
-close=close.sort_index()
 
 
 #stremlit
-ind=close.index
-ind1=ind.year
-syear =pd.DataFrame(ind1,columns=['Year'])
-syear=syear.drop_duplicates()
-#syear =pd.DataFrame(['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'],columns=['Year'])
+
+syear =pd.DataFrame(['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'],columns=['Year'])
 smonth=pd.DataFrame(['January','February','March','April','May','June','July','August','September','October','November','December'],columns=['Month'])
 
-
+stock = st.sidebar.text_input("Stock Ticker", 'aapl')
+ticker=stock
+apikey=os.getenv("apikey")
 option1 = st.sidebar.selectbox('Select a year',syear['Year'])
 option2 = st.sidebar.selectbox('Select a month',smonth['Month'])
 
@@ -46,7 +33,14 @@ year=option1
 
 filter=month+' '+year
 
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&apikey=%s&outputsize=full'%(ticker,apikey)
+r=requests.get(url)
+data=r.json()
 
+df=pd.DataFrame(data['Time Series (Daily)'])
+close=df.loc['4. close',:]
+close.index=pd.to_datetime(close.index,)
+close=close.sort_index()
 
 
 close0=pd.DataFrame(close)
