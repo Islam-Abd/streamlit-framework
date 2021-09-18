1672b05080967b0f31a56b5ba2eb2d748ebf54df
Islam-Abd <90225596+Islam-Abd@users.noreply.github.com>
2021-09-17 20:37:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,22 +8,16 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from datetime import datetime
 import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 #stremlit
-
 syear =pd.DataFrame(['2010','2011','2012','2013','2014','2015','2016','2017','2018','2019','2020','2021'],columns=['Year'])
 smonth=pd.DataFrame(['January','February','March','April','May','June','July','August','September','October','November','December'],columns=['Month'])
 
 stock = st.sidebar.text_input("Stock Ticker", 'aapl')
-ticker=stock
-apikey=os.getenv("apikey")
 option1 = st.sidebar.selectbox('Select a year',syear['Year'])
 option2 = st.sidebar.selectbox('Select a month',smonth['Month'])
 
@@ -33,15 +27,22 @@ year=option1
 
 filter=month+' '+year
 
+from datetime import datetime
+
+ticker=stock
+apikey=os.getenv("apikey")
+
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&apikey=%s&outputsize=full'%(ticker,apikey)
 r=requests.get(url)
 data=r.json()
 
+
+
 df=pd.DataFrame(data['Time Series (Daily)'])
 close=df.loc['4. close',:]
+
 close.index=pd.to_datetime(close.index,)
 close=close.sort_index()
-
 
 close0=pd.DataFrame(close)
 close0['4. close'] = close0['4. close'].astype(float)
@@ -52,5 +53,3 @@ import plotly.express as px
 
 fig = px.line(close1, y="Closing Price", title='Stock Closing Price',labels={"index": "Date"})
 st.plotly_chart(fig)
-
-
